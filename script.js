8b3eda55cbe0b77c850493ed220c3956d9652df6
Beatriz <bia_moraes2@hotmail.com>
2026-03-11 08:48:25 -0300

    primeiro commit pags

---
+++ script.js
@@ -0,0 +1,16 @@
+const cards = document.querySelectorAll(".card");
+
+window.addEventListener("scroll", () => {
+
+cards.forEach(card => {
+
+const top = card.getBoundingClientRect().top;
+
+if(top < window.innerHeight - 50){
+card.style.transform = "translateY(0)";
+card.style.opacity = "1";
+}
+
+});
+
+});
